5386b28d09049f5eaff6a6c06f66aed25ba06500
mawi <dagmawi20@gmail.com>
2017-11-30 00:08:52 -0500

    Created a new refrence migration

--- app/serializers/review_movie_serializer.rb
+++ app/serializers/review_movie_serializer.rb
@@ -1,3 +1,4 @@
 class ReviewMovieSerializer < ActiveModel::Serializer
   attributes :id, :rating, :movie_id, :user_id
+  belongs_to :Movies
 end
---
+++ db/migrate/20171130045053_addmovietoreview_movie.rb
@@ -0,0 +1,5 @@
+class AddmovietoreviewMovie < ActiveRecord::Migration[5.1]
+  def change
+    add_reference :movies, :review_movie, foreign_key: true
+  end
+end
