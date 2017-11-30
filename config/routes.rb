7630a643c0248baa3de975c636531a7f32862f13
mawi <dagmawi20@gmail.com>
2017-11-30 15:39:15 -0500

    fixed spelling error

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,11 @@ Rails.application.routes.draw do
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
 
-  patch '/review_movies/:id' => 'review_movies#update'
-  delete '/review_movies/:id' => 'review_movies#destroy'
-  get '/review_movie' => 'review_movies#index'
-  get '/review_movies/:id' => 'review_movies#show'
-  post '/review_movies/' => 'review_movies#create'
+  # patch '/review_movies/:id' => 'review_movies#update'
+  # delete '/review_movies/:id' => 'review_movies#destroy'
+  # get '/review_movie' => 'review_movies#index'
+  # get '/review_movies/:id' => 'review_movies#show'
+  # post '/review_movies/' => 'review_movies#create'
 
   resources :movies, except: %i[new edit]
   resources :users, only: %i[new edit]
